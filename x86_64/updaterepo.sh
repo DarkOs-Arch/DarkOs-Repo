9f7b4eb86ce96f253dc6c72403b57f83057334ab
r2dr0dn <r2dr0dn@pm.me>
2020-06-25 16:10:27 +0100

    Adding Nessacry Stuff

--- x86_64/updaterepo.sh
+++ x86_64/updaterepo.sh
@@ -5,6 +5,7 @@ rm DarkOs-Repo*
 
 echo "repo-add"
 repo-add -s -n -R DarkOs-Repo.db.tar.gz *.pkg.tar.xz
+repo-add -s -n -R DarkOs-Repo.db.tar.gz *.pkg.tar.zst
 #sleep 8
 
 echo "####################################"
