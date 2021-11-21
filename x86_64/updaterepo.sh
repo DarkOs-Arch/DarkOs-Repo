979a0c8f4e4910f1f8d131d7d29c08e1f40eebc7
ybenel <ybenel@molero.xyz>
2021-11-21 17:13:52 +0100

    Removing Symlink Issue

--- x86_64/updaterepo.sh
+++ x86_64/updaterepo.sh
@@ -8,6 +8,10 @@ repo-add -s -n -R DarkOs-Repo.db.tar.gz *.pkg.tar.xz
 repo-add -s -n -R DarkOs-Repo.db.tar.gz *.pkg.tar.zst
 #sleep 8
 
+mv DarkOs-Repo.db.tar.gz DarkOs-Repo.db
+mv DarkOs-Repo.db.tar.gz.sig DarkOs-Repo.db.sig
+mv DarkOs-Repo.files.tar.gz DarkOs-Repo.files
+mv DarkOs-Repo.files.tar.gz.sig DarkOs-Repo.files.sig
 echo "####################################"
 echo "Repo Updated!!"
 echo "####################################"
